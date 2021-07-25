18d56ca76fdad08c19149352d80fa71e30564eb1
Vikram Peddinti <vikram.peddinti@gmail.com>
2021-07-24 18:42:40 -0700

    Added ABV content scraper for bev

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 """
 Simple scraper: currently analyzing the sources we are using!
 """
+import re
 
 import requests
 from bs4 import BeautifulSoup
@@ -8,6 +9,43 @@ from bs4 import BeautifulSoup
 #Testing variables for a singular page
 babepage = 'https://drinkbabe.net/collections/wine/products/babe-100-rose'
 bevpage = 'https://drinkbev.com/products/rose-wine'
+
+#Dictionary storing all the scraped information
+bevinfo = {
+    "Flavor"     : '',
+    "ABV"        : '',
+    "Calories"   : '',
+    "Can_Size"   : '',
+    "Gluten_Free": '',
+    "Carbs"      : '',
+    "Sugar"      : '',
+    "Sizes"      : [],
+    "Costs"      : [],
+}
+
+#All the features we will be comparing are stored in these dictionaries
+babeinfo = {
+    "Flavor"     : '',
+    "ABV"        : '',
+    "Calories"   : '',
+    "Can_Size"   : '',
+    "Gluten_Free": '',
+    "Carbs"      : '',
+    "Sugar"      : '',
+    "Sizes"      : [],
+    "Costs"      : [],
+}
+
+"""
+Parse given text for abv information
+"""
+def parse_abv(pg_txt, soup):
+    #print(soup.find_all(attrs={'class': 'flavour-content'}))
+    #Works with bev | could be a faster way(iterating through above code and then finding regex?)
+    alcContent = soup.find_all(string=re.compile(r"(....)% ABV"))
+    print(alcContent)
+
+
 
 #send request and fetch the html page defined
 page = requests.get(
@@ -17,4 +55,6 @@ page = requests.get(
 soup = BeautifulSoup(page.content, 'html.parser')
 title = soup.title.text
 
-print(title)
+parse_abv(page, soup)
+
+
