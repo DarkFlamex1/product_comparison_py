1cd997e059c53240d2b947fd4c2171f6167ecefa
Vikram Peddinti <vikram.peddinti@gmail.com>
2021-07-25 10:42:42 -0700

    Cleaned up and refactored - pseudocode

---
+++ info_struct.py
@@ -0,0 +1,25 @@
+
+class info_struct:
+    """
+    This class represents the info for each product comparison.
+    """
+
+    # Dictionary storing all the scraped information
+    flavor_info = {
+        "Flavor": '',
+        "ABV": '',
+        "Calories": '',
+        "Can_Size": '',
+        "Gluten_Free": '',
+        "Carbs": '',
+        "Sugar": '',
+        "Sizes": [],
+        "Costs": [],
+    }
+
+    """
+    Helper function that sets the value in flavor info given a key and value.
+    No error checking, can be implemented later.
+    """
+    def set(self, key, value):
+        self.flavor_info.update(key, value);
--- scraper.py
+++ scraper.py
@@ -7,9 +7,14 @@ import requests
 from bs4 import BeautifulSoup
 
 #Testing variables for a singular page
-babepage = 'https://drinkbabe.net/collections/wine/products/babe-100-rose'
-bevpage = 'https://drinkbev.com/products/rose-wine'
+babepage_link = 'https://drinkbabe.net/collections/wine/products/babe-100-rose'
+bevpage_link = 'https://drinkbev.com/products/rose-wine'
 
+#Lists of each companies offerings - consists of info_struct objects
+bev_offerings = []
+babe_offerings = []
+
+""""
 #Dictionary storing all the scraped information
 bevinfo = {
     "Flavor"     : '',
@@ -34,11 +39,29 @@ babeinfo = {
     "Sugar"      : '',
     "Sizes"      : [],
     "Costs"      : [],
-}
+}"""
 
 """
 Parse given text for abv information
 """
+
+#Have a parent function that does the below on a button press!
+
+
+
+#Create functions that parse the offerings from the given links - test without iterator, add iterator if I have time
+#FAQ is needed for most of the information
+
+#Iterate through the main page, grabbing all the links of the offerings
+#Then go through each of the links filling out the info_struct given the designed code
+
+#Finally display each info_struct on a flask application, with tables or some clear split
+
+#write up focumentation as we go in comments, then see
+
+
+
+
 def parse_abv(pg_txt, soup):
     #print(soup.find_all(attrs={'class': 'flavour-content'}))
     #Works with bev | could be a faster way(iterating through above code and then finding regex?)
@@ -46,10 +69,10 @@ def parse_abv(pg_txt, soup):
     print(alcContent)
 
 
-
+#turn into function
 #send request and fetch the html page defined
 page = requests.get(
-    bevpage
+    bevpage_link
 )
 
 soup = BeautifulSoup(page.content, 'html.parser')
